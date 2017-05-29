660fd4d1e5de01217a515c2ef16e015a9ad2d873
ulkoart <ulkoart@gmail.com>
2017-05-29 16:17:19 +0300

    добавил replace_flag, если False то содержимое не переписывается

--- src/habraproxy.py
+++ src/habraproxy.py
@@ -22,11 +22,13 @@ def is_not_number(str):
 
 def replace_it(item):
     words = item.split()
+    replace_flag = False
     for position, word in enumerate(words):
         clean_word = word.strip('.,:!?@#$%^&*()_-')
         if len(clean_word) == 6 and is_not_number(word):
             words[position] = word.replace(clean_word, clean_word + "\u2122")
-    return ' '.join(words)
+            replace_flag = True
+    return replace_flag, ' '.join(words)
 
 
 @app.route('/', defaults={'path': ''})
@@ -46,7 +48,8 @@ def root_query(path):
     for item in content:
         if item.parent.name not in BAD_TAGS and not isinstance(item,Comment):
             replace_result = replace_it(item)
-            item.replaceWith(replace_result)
+            if replace_result[0]:
+                item.replaceWith(replace_result[1])
     return soup.prettify()
 
 
